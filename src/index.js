6b41c220a14eabd4d29001725e3c1283e4cd7a9a
masx200 <34191203+masx200@users.noreply.github.com>
2021-07-14 10:21:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import "element-remove";
 import "@babel/polyfill";
 
 import "@masx200/webpack-react-vue-spa-awesome-config/polyfill/src/index.js";
-import "https://polyfill.io/v3/polyfill.js?features=es2016%2Ces2017%2Ces2018%2Ces2019%2Ces2021";
+
+import "https://polyfill.io/v3/polyfill.js?features=es2017%2Ces2018%2Ces2019%2Ces2021";
